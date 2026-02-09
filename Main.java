71165e7c0be0f26f020f803c1914497c3c7b77c7
Camila <cdgolondrina@gmail.com>
2026-02-08 20:09:03 -0600

    guarda el arreglo exitosamente

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
+
 public class Main {
     public static void main(String[] args){
+        
         Integer[] numbers = RandomNumber.generateRandomNumbers(10);
+        
+        String filePath = "numeros.txt";
+        FileManager.writeToFile(filePath, numbers);
+
         System.out.println("Números aleatorios generados:");
         for(Integer num : numbers){
             System.out.println(num);
         }
+
+        System.out.println("Los números aleatorios han sido escritos en el archivo: " + filePath);
+
     }
 }
